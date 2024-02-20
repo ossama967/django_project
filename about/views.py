3d317d7b31b2b3361091030b39ffee5138046c28
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-20 23:57:00 +0000

    adding about app and adjusting the veiw.py for this app, settings.py and url.py of my_project

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+# Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page")
