b5f15935736b40a145903f601059d884761070cb
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-21 11:01:13 +0000

    adding the Heroku setting and gunicorn package

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,8 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ossama967-djangoproject-iv2wwalo5jn.ws-eu108.gitpod.io','.herokuapp.com', 'my-project-hello-world67-6ace87373ac1.herokuapp.com']
 
-https://my-project-hello-world67-6ace87373ac1.herokuapp.com/hello
-
 # Application definition
 
 INSTALLED_APPS = [
