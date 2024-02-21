a84c55b67c6f36d278c293881dee9ecbde1c4ea4
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-21 09:35:09 +0000

    adding the Heroku setting and gunicorn package

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yio-#fk8jw^j317m541$xt@^0v=15d84%8+396&4+v5_*fo-9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ossama967-djangoproject-iv2wwalo5jn.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-ossama967-djangoproject-iv2wwalo5jn.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
